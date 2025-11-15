7e4a4a663a646dfbb70937decd7838eaf8580757
Stuart Card <stuartcard450@gmail.com>
2025-11-14 23:54:57 -0700

    Got a basic help method working for the loggedOutClient

--- client/src/main/java/ui/LoggedOutClient.java
+++ client/src/main/java/ui/LoggedOutClient.java
@@ -14,19 +14,29 @@ public class LoggedOutClient {
 
     public void run() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to 240 chess");
+        System.out.println("Welcome to chess! Type \"help\" to get help!");
         printPrompt();
-
+        
         var result = "";
-        result = scanner.nextLine();
-        if (result.equals("hello")) {
-            System.out.println("Good to see you");
-        } else {
-            System.out.println("I don't know you");
+        String line = scanner.nextLine();
+        result = eval(line);
+    }
+
+    private String eval(String input) {
+        if (input.equals("help")) {
+            help();
         }
+        return "";
     }
 
     private void printPrompt() {
         System.out.print("\n" + SET_TEXT_COLOR_BLACK + ">>> " + SET_TEXT_COLOR_GREEN);
+    }
+
+    private void help() {
+        System.out.println("register <USERNAME> <PASSWORD> <EMAIL> - to create an account");
+        System.out.println("login <USERNAME> <PASSWORD> - to log in");
+        System.out.println("quit - to exit the program");
+        System.out.println("help - to print possible commands");
     }
 }
