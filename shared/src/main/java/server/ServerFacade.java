22ddd08ce86f990f1cb4f921040ed0b0b3216ebd
Stuart Card <stuartcard450@gmail.com>
2025-11-18 02:14:17 -0700

    added a lot of the request functionality in serverFacade

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,6 +1,14 @@
 package server;
 
+import com.google.gson.Gson;
+
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.*;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class ServerFacade {
     private final String serverUrl;
@@ -36,5 +44,31 @@ public class ServerFacade {
 
     public void observeGame(String gameID) {
 
+    }
+
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws Exception {
+        try {
+            return client.send(request, BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new Exception("Server error" + ex.getMessage());
+        }
     }
 }
