58f3cb346454b26f2918492cfbaaa2b8d1e89784
Stuart Card <stuartcard450@gmail.com>
2025-12-06 23:12:22 -0700

    Added to the websocket facade

--- client/src/main/java/ui/websocket/WebSocketFacade.java
+++ client/src/main/java/ui/websocket/WebSocketFacade.java
@@ -1,21 +1,59 @@
 package ui.websocket;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 import com.google.gson.Gson;
 import jakarta.websocket.*;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
 
-public class WebSocketFacade {
+public class WebSocketFacade extends Endpoint {
     private Session session;
     private final Gson gson = new Gson();
-            
-    public WebSocketFacade(String url) {
+    NotificationHandler notificationHandler;
+
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws Exception {
         try {
             url = url.replace("http", "ws");
             URI socketURI = new URI(url + "/ws");
-        } catch (URISyntaxException ex) {
+            this.notificationHandler = notificationHandler;
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
 
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
+                    notificationHandler.notify(serverMessage);
+                }
+            });
+        } catch (DeploymentException | URISyntaxException | IOException ex) {
+            String message = ex.getMessage();
+            throw new Exception("Problem with the websocket");
+        }
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+
+    public void joinGame(String authToken, Integer gameID) {
+        try {
+            UserGameCommand command = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameID);
+            this.session.getBasicRemote().sendText(gson.toJson(command));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void leaveGame(String authToken, Integer gameID) {
+        try {
+            UserGameCommand command = new UserGameCommand(UserGameCommand.CommandType.LEAVE, authToken, gameID);
+            this.session.getBasicRemote().sendText(gson.toJson(command));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
         }
     }
 }
