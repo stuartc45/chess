4963b228668923d2331ec08363bfe94716f16f3a
Stuart Card <stuartcard450@gmail.com>
2025-12-07 00:57:55 -0700

    Added a send message function in the backend to send a message to all the other connections in the websocket

--- server/src/main/java/server/websocket/Connections.java
+++ server/src/main/java/server/websocket/Connections.java
@@ -1,7 +1,9 @@
 package server.websocket;
 
 import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
 
+import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Connections {
@@ -13,5 +15,16 @@ public class Connections {
 
     public void close(Session session) {
         connections.remove(session);
+    }
+
+    public void sendMessage(Session session, ServerMessage message) throws IOException {
+        String msg = message.toString();
+        for (Session c : connections.values()) {
+            if (c.isOpen()) {
+                if (!c.equals(session)) {
+                    c.getRemote().sendString(msg);
+                }
+            }
+        }
     }
 }
